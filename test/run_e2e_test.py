8d34a89f600e0201c09971ebf5d2e5cf29c40a8d
Devin Bonnie <dbbonnie@amazon.com>
2020-01-29 16:14:50 -0800

    Address review comments
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>

--- test/run_e2e_test.py
+++ test/run_e2e_test.py
@@ -1,9 +1,10 @@
 """
 End to end tests for the system_metrics_collector ROS2 package.
 
-These tests assume
+These tests assume that the relevant ROS2 install and project has been sourced.
 """
 
+import logging
 import os
 import signal
 import subprocess
@@ -24,10 +25,19 @@ EXPECTED_LIFECYCLE_NODES = ['/linux_system_cpu_collector',
                             '/listener_process_memory_node',
                             '/talker_process_cpu_node',
                             '/talker_process_memory_node']
-
 EXPECTED_LIFECYCLE_STATE = 'active [3]'
 LAUNCH_COMMAND = 'ros2 launch system_metrics_collector talker_listener_example.launch.py'
+LIST_LIFECYCLE_NODES_COMMAND = 'ros2 lifecycle nodes'
 TIMEOUT_SECONDS = 20
+QOS_DEPTH = 1
+RETURN_VALUE_FAILURE = 1
+RETURN_VALUE_SUCCESS = 1
+
+
+class SystemMetricsEnd2EndTestException(Exception):
+    """Exception used to denote end to end test failures."""
+
+    pass
 
 
 class StatisticsListener(Node):
@@ -39,7 +49,7 @@ class StatisticsListener(Node):
         self.sub = self.create_subscription(MetricsMessage,
                                             'system_metrics',
                                             self.listener_callback,
-                                            1)
+                                            QOS_DEPTH)
         self.future = future
         self.received_all_published_stats = False
         self.expected_lifecycle_nodes = expected_lifecycle_nodes
@@ -48,6 +58,9 @@ class StatisticsListener(Node):
         """
         Handle published MetricsMessages.
 
+        Checks each received message measurement_source_name against a list of
+        expected_lifecycle_nodes and marks the future field as done if all sources
+        have been observed.
         :param msg: received message
         :return: None
         """
@@ -61,45 +74,29 @@ class StatisticsListener(Node):
             self.future.set_result(self.received_all_published_stats)
 
 
-def print_test(message: str) -> None:
-    """
-    Print a formatted string for easier parsing.
-
-    :param message: string to be printed
-    :return:
-    """
-    print('========================')
-    print(message)
-    print('========================')
-
-
 def check_for_expected_nodes(args=None) -> None:
     """
     Check that all expected nodes can be found.
 
     :param args:
-    :return:
     """
-    expected_nodes = ['/listener', '/talker'] + EXPECTED_LIFECYCLE_NODES
+    try:
+        rclpy.init(args=args)
+        node = rclpy.create_node('check_for_expected_nodes_test')
 
-    rclpy.init(args=args)
-    node = rclpy.create_node('check_for_expected_nodes_test')
-    actual_nodes = node.get_node_names_and_namespaces()
-    node.destroy_node()
-    rclpy.shutdown()
+        expected_nodes = ['/listener', '/talker'] + EXPECTED_LIFECYCLE_NODES
+        observed_nodes = node.get_node_names_and_namespaces()
+        # observed_nodes node does not contain the preceeding '/'
+        observed_nodes = ['/' + node[0] for node in observed_nodes]
 
-    expected_count = len(expected_nodes)
-    actual_count = 0
-    for item in actual_nodes:
-        found_node = '/' + item[0]
-        if found_node in expected_nodes:
-            actual_count += 1
-            expected_nodes.remove(found_node)
-
-    if expected_count != actual_count:
-        raise Exception('Failed to enumerate expected nodes. Found #' + actual_count + ': ', actual_nodes)
-    else:
-        print_test('check_for_expected_nodes success')
+        if set(expected_nodes).issubset(set(observed_nodes)):
+            logging.info('check_for_expected_nodes success')
+        else:
+            raise SystemMetricsEnd2EndTestException('Failed to enumerate expected nodes.'
+                                                    ' Found: ', observed_nodes)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 def check_lifecycle_node_enumeration() -> None:
@@ -107,14 +104,16 @@ def check_lifecycle_node_enumeration() -> None:
     Check that all lifecycle nodes exist.
 
     This requires the ros2lifecycle dependency.
-    :return:
     """
-    stream = os.popen('ros2 lifecycle nodes')
-    output = stream.read().splitlines()
-    if output != EXPECTED_LIFECYCLE_NODES:
-        raise Exception('check_lifecycle_node_enumeration failed: ' + str(output))
+    output = subprocess.check_output(LIST_LIFECYCLE_NODES_COMMAND.split(' '),
+                                     timeout=TIMEOUT_SECONDS)\
+        .decode(sys.stdout.encoding).splitlines()
+
+    if output == EXPECTED_LIFECYCLE_NODES:
+        logging.info('check_lifecycle_node_enumeration success')
     else:
-        print_test('check_lifecycle_node_enumeration success')
+        raise SystemMetricsEnd2EndTestException('check_lifecycle_node_enumeration failed: '
+                                                + str(output))
 
 
 def check_lifecycle_node_state() -> None:
@@ -122,19 +121,25 @@ def check_lifecycle_node_state() -> None:
     Check that each lifecycle node is active.
 
     This requires the ros2lifecycle dependency.
-    :return:
     """
     for lifecycle_node in EXPECTED_LIFECYCLE_NODES:
-        stream = os.popen('ros2 lifecycle get ' + str(lifecycle_node))
-        output = stream.read().rstrip()
+        output = subprocess.check_output(['ros2', 'lifecycle', 'get', str(lifecycle_node)],
+                                         timeout=TIMEOUT_SECONDS)\
+            .decode(sys.stdout.encoding).splitlines()
 
-        if output != EXPECTED_LIFECYCLE_STATE:
-            raise Exception('check_lifecycle_node_state:'
-                            + lifecycle_node +
-                            ' not in expected state: '
-                            + str(output))
+        if not output:
+            raise SystemMetricsEnd2EndTestException('check_lifecycle_node_state: Unexpected output'
+                                                    ' for node: ' + lifecycle_node)
 
-    print_test('check_lifecycle_node_state success')
+        if output[0] == EXPECTED_LIFECYCLE_STATE:
+            logging.debug(lifecycle_node + ' in expected state')
+        else:
+            raise SystemMetricsEnd2EndTestException('check_lifecycle_node_state:'
+                                                    + lifecycle_node +
+                                                    ' not in expected state: '
+                                                    + str(output))
+
+    logging.info('check_lifecycle_node_state success')
 
 
 def check_for_statistic_publications(args=None) -> None:
@@ -142,20 +147,33 @@ def check_for_statistic_publications(args=None) -> None:
     Check that all nodes publish a statistics message.
 
     :param args:
-    :return:
     """
-    future = Future()
-    rclpy.init(args=args)
-    node = StatisticsListener(future, EXPECTED_LIFECYCLE_NODES)
-    rclpy.spin_until_future_complete(node, future, timeout_sec=TIMEOUT_SECONDS)
-    node.destroy_node()
-    rclpy.shutdown()
+    try:
+        future = Future()
+        rclpy.init(args=args)
+        node = StatisticsListener(future, set(EXPECTED_LIFECYCLE_NODES))
+        rclpy.spin_until_future_complete(node, future, timeout_sec=TIMEOUT_SECONDS)
 
-    if not node.received_all_published_stats:
-        raise Exception('check_for_statistic_publications failed. Absent publisher(s): '
-                        + str(node.expected_lifecycle_nodes))
-    else:
-        print_test('check_for_statistic_publications success')
+        if node.received_all_published_stats:
+            logging.info('check_for_statistic_publications success')
+        else:
+            raise SystemMetricsEnd2EndTestException('check_for_statistic_publications failed.'
+                                                    ' Absent publisher(s): '
+                                                    + str(node.expected_lifecycle_nodes))
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+def setup_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('[%(module)s] [%(levelname)s] [%(asctime)s]: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 
 def main(args=None) -> int:
@@ -165,31 +183,39 @@ def main(args=None) -> int:
     :param args:
     :return: 0 if all tests pass, 1 if any fail
     """
-    retval = 1
-    split_command = LAUNCH_COMMAND.split()
-    print_test('Executing: ' + str(split_command))
-    process = subprocess.Popen(split_command)
-    time.sleep(2)
-
     try:
-        print_test('Starting tests')
+        setup_logger()
+
+        return_value = RETURN_VALUE_FAILURE
+        split_command = LAUNCH_COMMAND.split()
+        logging.info('Executing: ' + str(split_command))
+        process = subprocess.Popen(split_command)
+        time.sleep(2)
+
+        logging.info('Starting tests')
+
         check_for_expected_nodes()
         check_lifecycle_node_enumeration()
         check_lifecycle_node_state()
         check_for_statistic_publications(args)
-        print_test('All tests succeeded')
-        retval = 0
-    except Exception as e:
-        print_test('Caught exception: ' + str(e))
-        retval = 1
-    finally:
-        print_test('Finished tests. Sending SIGINT')
-        os.kill(process.pid, signal.SIGINT)
-        time.sleep(2)
 
-    return retval
+        logging.info('All tests succeeded')
+        return_value = RETURN_VALUE_SUCCESS
+
+    except SystemMetricsEnd2EndTestException:
+        logging.error('Test failure: ', exc_info=True)
+
+    except Exception:
+        logging.error('Caught unrelated error: ', exc_info=True)
+
+    finally:
+        logging.info('Finished tests. Sending SIGINT')
+        os.kill(process.pid, signal.SIGINT)
+        process.wait(timeout=TIMEOUT_SECONDS)
+
+    return return_value
 
 
 if __name__ == '__main__':
-    retval = main()
-    sys.exit(retval)
+    test_output = main()
+    sys.exit(test_output)
