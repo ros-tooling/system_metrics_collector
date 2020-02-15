42e78e30fe9ee0324f608c98b9e3ded8419a4337
Devin Bonnie <dbbonnie@amazon.com>
2020-02-14 17:17:23 -0800

    Update script docstring
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>

--- test/run_e2e_test.sh
+++ test/run_e2e_test.sh
@@ -2,8 +2,8 @@
 # This script sources the built ROS2 workspace and executes the python end to end tests
 # for the system_metrics_collector package.
 #
-# The goal of this script is to independently run tests, as a canary, on the master branch
-# and ultimately released APT packages.
+# The goal of this script is to independently run end to end tests, as a canary,
+# on the master branch and ultimately released APT packages.
 
 ROS_WS_DIR=ros_ws/install
 TEST_DIR=ros_ws/src/system_metrics_collector/test
